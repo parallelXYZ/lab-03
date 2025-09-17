73ab76b07fd5d1fe95938212a075d8257ef38b57
parallelXYZ <lnone67@gmail.com>
2025-09-16 18:23:55 -0600

    done lab 3

---
+++ ListyCityLab3/app/src/main/java/com/example/listycitylab3/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.listycitylab3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements AddCityFragment.AddCityDialogListener {
+
+    private ArrayList<City> dataList;
+    private ListView cityList;
+    private CityArrayAdapter cityAdapter;
+    @Override
+    public void addCity(City city) {
+        cityAdapter.add(city);
+        cityAdapter.notifyDataSetChanged();
+    }
+    @Override
+    public void editCity(City city, String name, String province) {
+        city.setName(name);
+        city.setProvince(province);
+        cityAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] cities = {"Edmonton", "Vancouver", "Toronto"};
+        String[] provinces = { "AB", "BC", "ON" };
+
+        dataList = new ArrayList<City>();
+        for (int i = 0; i < cities.length; i++) {
+            dataList.add(new City(cities[i], provinces[i]));
+        }
+
+        cityList = findViewById(R.id.city_list);
+        cityAdapter = new CityArrayAdapter(this, dataList);
+        cityList.setAdapter(cityAdapter);
+
+        FloatingActionButton fab = findViewById(R.id.button_add_city);
+        fab.setOnClickListener(v -> {
+            new AddCityFragment().show(getSupportFragmentManager(), "Add City");
+        });
+
+        cityList.setOnItemClickListener((parent, view, position, id) -> {
+            City selected = (City) parent.getItemAtPosition(position);
+//            Log.d("Debug", selected.toString());
+            AddCityFragment editCity = AddCityFragment.newInstance(selected);
+            editCity.show(getSupportFragmentManager(), "Edit City");
+        });
+    }
+}
